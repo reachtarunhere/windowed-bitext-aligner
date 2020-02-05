fff0195902642040e213f4853f6a04549cdc6b0b
Tarun Kumar Vangani <tarunkv@i2r.a-star.edu.sg>
2020-02-05 20:28:04 +0700

    added req function names without bodies

--- scoring.py
+++ scoring.py
@@ -1,3 +1,15 @@
-def sentence_len_ratio(s1, s2):
+def sentence_len_ratio(s1, s2, cos_scores):
     l1, l2 = len(s1.split()), len(s2.split())
     return l1/l2 if l1 > l2 else l2/l1
+
+
+def cos_second_margin(s1, s2, cos_scores):
+    pass
+
+
+def cos_avg_margin(s1, s2, cos_scores):
+    pass
+
+
+def cos_avg_ratio_margin(s1, s2, cos_scores):
+    pass
