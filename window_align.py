72b663b37b83098451c3a5bd8ba4c3438282567d
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-02-05 19:53:36 +0700

    implement pair finder

--- window_align.py
+++ window_align.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def CandidateGenerator(trg_max, window_size=5):
     """ trg_max is len of trg sentences file"""
 
@@ -9,6 +12,17 @@ def CandidateGenerator(trg_max, window_size=5):
         return [x for x in range(i-half_margin, i+half_margin+1) if valid_trg(x)]
 
     return gen_candidate
+
+
+def PairScoreFinder(dot_scores, get_candidate_fn):
+
+    def get_score(src_i):
+        candidate_indexes = get_candidate_fn(src_i)
+        candidate_scores = dot_scores[src_i][np.array(candidate_indexes)]
+        best_cand_index = candidate_indexes[candidate_scores.argmax()]
+        return best_cand_index, candidate_scores
+
+    return get_score
 
 
 def scoring_matrix(emb_src, emb_tgt):
