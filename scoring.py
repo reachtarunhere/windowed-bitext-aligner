80f1ab3a0bd9d899f180cf7b7cecc29efeb806fb
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-02-27 03:19:13 +0700

    margin_second_largest implemented

--- scoring.py
+++ scoring.py
@@ -1,10 +1,11 @@
-def sentence_len_ratio(s1, s2, cos_scores):
+def sentence_len_ratio(s1, s2):
     l1, l2 = len(s1.split()), len(s2.split())
     return l1/l2 if l1 > l2 else l2/l1
 
 
-def cos_second_margin(s1, s2, cos_scores):
-    pass
+def margin_second_largest(cos_scores):
+    sorted_scores = np.sort(cos_scores)
+    return sorted_scores[-1]-sorted_scores[-2]
 
 
 def cos_avg_margin(s1, s2, cos_scores):
